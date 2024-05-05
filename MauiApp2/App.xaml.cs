90502f6f344ad58aa9cef11db57576e87ed375c9
zschmitt3 <zschmitt3@my.wctc.edu>
2024-05-05 13:53:38 -0500

    Initial

---
+++ MauiApp2/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿namespace MauiApp2
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+            UserAppTheme = PlatformAppTheme;
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+            window.Title = "MauiApp2";
+            return window;
+        }
+    }
+}
---
+++ MauiApp2/Imports.cs
@@ -0,0 +1,5 @@
+global using MauiApp2;
+global using MauiApp2.Views;
+
+// Static
+global using static Microsoft.Maui.Graphics.Colors;
